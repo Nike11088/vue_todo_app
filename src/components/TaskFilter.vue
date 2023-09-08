c79c98b81285ea4695b4c1c1d42048f54ffe9927
Nikita Abakumov <nabakumov@mt.1tv.ru>
2023-09-08 16:03:38 +0300

    added active filter button style

--- src/components/TaskFilter.vue
+++ src/components/TaskFilter.vue
@@ -2,21 +2,21 @@
   <div>
     <div class="flex justify-between items-center w-[330px] h-[40px] mt-5">
       <button 
-        class="w-[110px] h-[40px] text-blue-600 font-medium border-2 border-blue-700 rounded-l-full border-r-0 hover:bg-blue-300"  
+        class="w-[110px] h-[40px] text-blue-600 font-medium border-2 border-blue-700 rounded-l-full border-r-0 active:bg-none hover:bg-blue-300"  
         :class="{ 'text-white bg-blue-600 hover:bg-blue-500': activeFilter === 'All' }"  
         @click="setFilter('All')"    
       >
         All
       </button>
       <button 
-        class="w-[110px] h-[40px] text-blue-600 font-medium border-2 border-blue-700 hover:bg-blue-300 border-lr-2"
+        class="w-[110px] h-[40px] text-blue-600 font-medium border-2 border-blue-700 active:bg-none hover:bg-blue-300 border-lr-2"
         :class="{ 'text-white bg-blue-600 hover:bg-blue-500': activeFilter === 'Active' }" 
         @click="setFilter('Active')"  
       >
         Active
       </button>
       <button 
-        class="w-[110px] h-[40px]  text-blue-600 font-medium border-2 border-blue-700 hover:bg-blue-300 rounded-r-full border-l-0"
+        class="w-[110px] h-[40px]  text-blue-600 font-medium border-2 border-blue-700 active:bg-none hover:bg-blue-300 rounded-r-full border-l-0"
         :class="{ 'text-white bg-blue-600 hover:bg-blue-500': activeFilter === 'Done' }"  
         @click="setFilter('Done')" 
       >
