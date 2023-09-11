667e8c1398e5a440f534e7374c5da0b4519ef39d
Nikita Abakumov <nabakumov@mt.1tv.ru>
2023-09-11 15:33:46 +0300

    click complete and delete only on visible icons

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -4,34 +4,37 @@
     @mouseleave="mouseLeave"
   >
     <div class="flex items-center hover:cursor-pointer">
-      <div @click="completeTask">
-          <span 
-          class="material-icons-round !text-3xl mr-1 text-green-400 hover:text-green-600"
-          :class="{'collapse': !task?.completed}"       
-        >
-          done
-        </span>
-      </div>                   
+      <span 
+        class="material-icons-round !text-3xl mr-1 text-green-400 hover:text-green-600 collapse"
+        :class="{'!visible': completeVisible || task?.completed}" 
+        @click="completeTask"      
+      >
+        done
+      </span>                 
       <span
         :class="{'line-through': task.completed}"
       >
         {{ task.text }}
       </span>
-    </div>     
-    <div @click="deleteTask">
-      <span 
-        class="material-icons-outlined !text-3xl ml-1 text-red-400 hover:text-red-600 hover:cursor-pointer"
-        :class="{'collapse': !deleteVisible}"
-      >
-        delete
-      </span> 
-    </div>   
+    </div> 
+    <span 
+      class="material-icons-outlined !text-3xl ml-1 text-red-400 hover:text-red-600 hover:cursor-pointer collapse"
+      :class="{'!visible': deleteVisible}"
+      @click="deleteTask"
+    >
+      delete
+    </span>  
   </div> 
 </template>
 
 <script lang="ts">
 import { type PropType, defineComponent } from 'vue';
 import { type Task } from '../types/Task';
+
+interface State {
+  completeVisible: boolean,
+  deleteVisible: boolean
+}
 
 export default defineComponent({ 
   props: {
@@ -40,18 +43,22 @@ export default defineComponent({
       required: true
     }
   },
-  data () {
+  data () : State {
     return {
+      completeVisible: false,
       deleteVisible: false
     }
   },
   methods: {
     mouseOver () { 
+      this.completeVisible = true
       this.deleteVisible = true
     },
     mouseLeave () {
+      this.completeVisible = false
       this.deleteVisible = false
     },
+    
     completeTask () {
       this.$emit('complete', this.task.id)
     },
