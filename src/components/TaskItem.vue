b70735d400b209eabd3331679b72d64470fa4dbe
Nikita Abakumov <nabakumov@mt.1tv.ru>
2023-09-11 17:17:14 +0300

    added mobile check

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,29 +1,37 @@
 <template>
-  <div class="flex justify-between items-center border-2 border-blue-700 rounded-xl w-full px-2 mb-2"
+  <div class="flex justify-between items-center border-2 border-blue-700 rounded-xl w-full py-1 px-2 mb-2"
     @mouseover="mouseOver"
-    @mouseleave="mouseLeave"
+    @mouseleave="mouseLeave"    
   >
-    <div class="flex items-center hover:cursor-pointer">
-      <span 
-        class="material-icons-round !text-3xl mr-1 text-green-400 hover:text-green-600 collapse"
-        :class="{'!visible': completeVisible || task?.completed}" 
-        @click="completeTask"      
-      >
-        done
-      </span>                 
+    <div 
+      class="flex items-center hover:cursor-pointer"
+      @click="clickTask"
+    >
+      <div class="flex items-center w-[30px] h-[30px] mr-1">
+        <span 
+          ref="completeIcon"
+          v-if="completeVisible || task?.completed "
+          class="material-icons-round !text-3xl text-green-400 hover:text-green-600"              
+        >
+          done
+        </span> 
+      </div>                    
       <span
-        :class="{'line-through': task.completed}"
+        :class="{'line-through': task.completed}"        
       >
         {{ task.text }}
       </span>
     </div> 
-    <span 
-      class="material-icons-outlined !text-3xl ml-1 text-red-400 hover:text-red-600 hover:cursor-pointer collapse"
-      :class="{'!visible': deleteVisible}"
-      @click="deleteTask"
-    >
-      delete
-    </span>  
+    <div class="flex items-center w-[30px] h-[30px] mr-1">
+      <span  
+        ref="deleteIcon"
+        v-if="deleteVisible"
+        class="material-icons-outlined !text-3xl ml-1 text-red-400 hover:text-red-600 hover:cursor-pointer"
+        @click="deleteTask"
+      >
+        delete
+      </span> 
+    </div>    
   </div> 
 </template>
 
@@ -51,19 +59,32 @@ export default defineComponent({
   },
   methods: {
     mouseOver () { 
+      if (this.isMobile()) return
       this.completeVisible = true
       this.deleteVisible = true
     },
     mouseLeave () {
       this.completeVisible = false
       this.deleteVisible = false
-    },
-    
-    completeTask () {
+    },    
+    completeTask () {      
       this.$emit('complete', this.task.id)
     },
     deleteTask () {
+      if (!this.$refs.deleteIcon.classList.contains('!visible')) return
       this.$emit('delete', this.task.id)
+    },
+    isMobile () {
+      return /Android|iPhone/i.test(navigator.userAgent)
+    },
+    clickTask () {
+      if (this.isMobile()) {
+        this.completeVisible = !this.completeVisible
+        this.deleteVisible = !this.deleteVisible
+        return
+      }
+
+      this.completeTask()
     }
   },
   emits: {
